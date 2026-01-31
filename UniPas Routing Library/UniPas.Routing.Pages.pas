a0cf2e5580b134444b2114ce4f8fcf17f9781ea0
Shaun Roselt <Me@ShaunRoselt.com>
2026-02-01 01:54:42 +0200

    Revert UniPas.Routing.Pages.pas to previous commit state

--- UniPas Routing Library/UniPas.Routing.Pages.pas
+++ UniPas Routing Library/UniPas.Routing.Pages.pas
@@ -4,34 +4,15 @@ interface
 
 uses
   System.Classes,
-  UniPas.uPage_404PageNotFound,
-  uPage_Home,
-  uPage_Achievements,
-  uPage_Game,
-  uPage_MatchHistory,
-  uPage_Options,
-  uPage_Statistics;
+  UniPas.uPage_404PageNotFound;
 
 const
-  PagesArray: TArray<String> = ['404PageNotFound',
-                                'Home',
-                                'Achievements',
-                                'Game',
-                                'MatchHistory',
-                                'Options',
-                                'Statistics'];
+  PagesArray: TArray<String> = ['404PageNotFound'];
 
 implementation
 
 initialization
   // Register all page frame classes in one place.
-  RegisterClasses([TPage_404PageNotFound,
-                   TPage_Home,
-                   TPage_Achievements,
-                   TPage_Game,
-                   TPage_MatchHistory,
-                   TPage_Options,
-                   TPage_Statistics
-                  ]);
+  RegisterClasses([TPage_404PageNotFound]);
 
 end.
